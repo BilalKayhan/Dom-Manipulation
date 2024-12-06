3b005e4ccdfcd4da5374b4305c5dfb27872b308e
BilalKayhan <fbilalkayhan@gmail.com>
2024-12-06 19:16:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const header = document.querySelector("#header");
+header.style.backgroundColor = "blue";
+header.innerHTML = "BİLAL KAYHAN";
+header.style.border = "4px solid yellow";
+
+const pageTitle = document.querySelector("#page_title");
+pageTitle.style.backgroundColor = "red";
+pageTitle.innerHTML = "TÜRKİYE";
+pageTitle.style.border = "4px solid white";
+
+const content = document.querySelector("#content");
+content.style.backgroundColor = "purple";
+content.innerHTML = "DOM MANIPULATION";
+content.style.border = "4px solid grey";
+
+const footer = document.querySelector("#footer");
+footer.style.backgroundColor = "white";
+footer.style.color = "yellow"
+footer.innerHTML = "AKIN CENGİZ";
+footer.style.border = "4px solid black";
+
+const container = document.querySelector(".container");
+container.style.backgroundColor = "green"
+container.style.border = "4px solid aqua"
+
